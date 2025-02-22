d75a56e873eed0adbda8290f84ca86ae9c674e85
Alexis <ale.maldo097@gmail.com>
2025-02-22 11:02:50 -0300

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,6 +4,7 @@ const api = axios.create({
     baseURL: process.env.REACT_APP_API_BASE_URL,
     headers: {
         "Content-Type": "application/json",
+        'Accept': 'application/json',
     },
 });
 export default api;
